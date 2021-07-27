54991300faf47001628509d300ddf049d7bb00d1
fiansyah31 <syahputraalfian223@gmail.com>
2021-07-27 14:24:15 +0700

    edit resolusi

--- js.js
+++ js.js
@@ -1,11 +1,4 @@
-function actionbarlef() {
-  var x = document.getElementById("barlef");
-  if (x.className === "section-bar-left") {
-    x.className += " responsive";
-  } else {
-    x.className = "section-bar-left";
-  }
-}
+
 
  $('#barlef').click(function (e){
        e.preventDefault();
